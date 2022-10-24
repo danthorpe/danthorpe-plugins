6b0bc90847e9d67faa9769fa7c556e76bacd6bbe
Daniel Thorpe <309420+danthorpe@users.noreply.github.com>
2022-10-24 09:04:21 +0100

    Adds Protobuf/gRPC plugin (#2)
    Co-authored-by: danthorpe <danthorpe@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -2,43 +2,79 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "danthorpe-plugins",
-    platforms: [
-        .macOS(.v12)
-    ],
-    products: [
-        .plugin(name: "SwiftLint", targets: ["SwiftLintPlugin"]),
-        .plugin(name: "SwiftLintAutocorrect", targets: ["SwiftLintAutocorrectPlugin"]),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
-        ),
-        .plugin(
-            name: "SwiftLintPlugin",
-            capability: .buildTool(),
-            dependencies: [
-                "SwiftLintBinary"
-            ]
-        ),
-        .plugin(
-            name: "SwiftLintAutocorrectPlugin",
-            capability: .command(
-                intent: .custom(
-                    verb: "swiftlint",
-                    description: "Invokes swiftlint --autocorrect, which will fix all correctable violations."
-                ),
-                permissions: [
-                    .writeToPackageDirectory(reason: "All correctable violations are fixed by SwiftLint.")
-                ]
-            ),
-            dependencies: [
-                "SwiftLintBinary"
-            ]
-        )
+var package = Package(name: "danthorpe-plugins")
 
-    ]
-)
+package.dependencies = [
+    .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-protobuf", from: "1.20.2"),
+]
+
+// MARK: - Names
+
+let SwiftLint = "SwiftLint"
+let SwiftLintBinary = "SwiftLintBinary"
+let SwiftLintAutocorrect = "SwiftLintAutocorrect"
+let Protobuf = "Protobuf"
+
+// MARK: Products
+
+package.products = [
+    .plugin(name: SwiftLint, targets: [SwiftLint]),
+    .plugin(name: SwiftLintAutocorrect, targets: [SwiftLintAutocorrect]),
+    .plugin(name: Protobuf, targets: [Protobuf]),
+]
+
+// MARK: Targets
+
+extension Target {
+
+    static let swiftLintBinary: Target = .binaryTarget(
+        name: SwiftLintBinary,
+        url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
+        checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
+    )
+
+    static let swiftLint: Target = .plugin(
+        name: SwiftLint,
+        capability: .buildTool(),
+        dependencies: [
+            .swiftLintBinary
+        ]
+    )
+
+    static let swiftLintAutocorrect: Target = .plugin(
+        name: SwiftLintAutocorrect,
+        capability: .buildTool(),
+        dependencies: [
+            .swiftLintBinary
+        ]
+    )
+
+    static let protobuf: Target = .plugin(
+        name: Protobuf,
+        capability: .buildTool(),
+        dependencies: [.protoc, .gRPC]
+    )
+}
+
+package.targets = [
+    .swiftLintBinary,
+    .swiftLint,
+    .swiftLintAutocorrect,
+    .protobuf
+]
+
+
+// MARK: - Dependencies
+
+extension Target.Dependency {
+    static let swiftLintBinary: Target.Dependency = .target(
+        name: SwiftLintBinary
+    )
+    static let protoc: Target.Dependency = .product(
+        name: "protoc-gen-swift", package: "swift-protobuf"
+    )
+    static let gRPC: Target.Dependency = .product(
+        name: "protoc-gen-grpc-swift", package: "grpc-swift"
+    )
+}
